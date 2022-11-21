9af86285a48e0c0076abdf49e1209b78204b930d
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2022-11-20 22:00:38 -0500

    ff

---
+++ EOC_and_TAN_Abstraction_Polymorphism_Demo_PE/Pets/Bird.cs
@@ -0,0 +1,25 @@
+﻿using Abstraction_Polymorphism_Demo.Pets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Polymorphism_Demo
+{
+    internal class Bird : Pet
+    {
+        //Identical child contructor
+        //type = bird
+        public Bird(string name, DateTime birthday)
+            : base(name, birthday, "bird")
+        {
+        }
+        
+        //"{Name} says SQUAWK!"
+        public override void Speak()
+        {
+            Console.WriteLine(this.Name + " says SQUAWK!");
+        }
+    }
+}
---
+++ EOC_and_TAN_Abstraction_Polymorphism_Demo_PE/Shapes/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using Abstraction_Polymorphism_Demo.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Polymorphism_Demo
+{
+    internal class Triangle : Shape
+    {
+        //(base =  bottom) bottom and height
+        private double height;
+        private double bottom;
+
+        //Create a triangle of a set base and height
+        public Triangle(double height, double bottom) : base("triangle")
+        {
+            this.height = height;
+            this.bottom = bottom;
+        }
+
+        //Override the CalculateArea method
+        //Area of a triangle is (b*h)/2
+        public override double CalculateArea()
+        {
+            return height * bottom * 0.5;
+        }
+
+        //Override the Area property
+        //Area of a triangle is (b*h)/2
+        public override double Area
+        {
+            get { return height * bottom * 0.5; }
+        }
+    }
+}
